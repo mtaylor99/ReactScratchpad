6b555c7258217995ecc621f476462d3099cb4e25
Mathew Taylor <mathew.taylor@codebase8.com>
2020-12-23 08:30:09 +0000

    Client app now runs

--- client/src/API.ts
+++ client/src/API.ts
@@ -1,15 +1,14 @@
-import axios, { AxiosResponse } from "axios"
+import axios, { AxiosResponse } from 'axios'
 
-const baseUrl: string = "http://localhost:4000"
+const baseUrl: string = 'http://localhost:4000'
 
 export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
   try {
     const todos: AxiosResponse<ApiDataType> = await axios.get(
-      baseUrl + "/todos"
+      baseUrl + '/todos'
     )
     return todos
-  }
-  catch (error) {
+  } catch (error) {
     throw new Error(error)
   }
 }
@@ -18,18 +17,17 @@ export const addTodo = async (
   formData: ITodo
 ): Promise<AxiosResponse<ApiDataType>> => {
   try {
-    const todo: Omit<ITodo, "_id"> = {
+    const todo: Omit<ITodo, '_id'> = {
       name: formData.name,
       description: formData.description,
       status: false,
     }
     const saveTodo: AxiosResponse<ApiDataType> = await axios.post(
-      baseUrl + "/add-todo",
+      baseUrl + '/add-todo',
       todo
     )
     return saveTodo
-  }
-  catch (error) {
+  } catch (error) {
     throw new Error(error)
   }
 }
@@ -38,7 +36,7 @@ export const updateTodo = async (
   todo: ITodo
 ): Promise<AxiosResponse<ApiDataType>> => {
   try {
-    const todoUpdate: Pick<ITodo, "status"> = {
+    const todoUpdate: Pick<ITodo, 'status'> = {
       status: true,
     }
     const updatedTodo: AxiosResponse<ApiDataType> = await axios.put(
@@ -46,8 +44,7 @@ export const updateTodo = async (
       todoUpdate
     )
     return updatedTodo
-  }
-  catch (error) {
+  } catch (error) {
     throw new Error(error)
   }
 }
@@ -57,11 +54,10 @@ export const deleteTodo = async (
 ): Promise<AxiosResponse<ApiDataType>> => {
   try {
     const deletedTodo: AxiosResponse<ApiDataType> = await axios.delete(
-      `${baseUrl}/delete-todo/${_id}`,
+      `${baseUrl}/delete-todo/${_id}`
     )
     return deletedTodo
-  }
-  catch (error) {
+  } catch (error) {
     throw new Error(error)
   }
 }
--- client/src/type.d.ts
+++ client/src/type.d.ts
@@ -1,19 +1,20 @@
 interface ITodo {
-  _id: string
-  name: string
-  description: string
-  status: boolean
-  createdAt?: string
-  updatedAt?: string
+    _id: string
+    name: string
+    description: string
+    status: boolean
+    createdAt?: string
+    updatedAt?: string
 }
 
-interface TodoProps {
-  todo: ITodo
+type TodoProps = {
+    todo: ITodo
 }
 
 type ApiDataType = {
-  message: string
-  status: string
-  todos: ITodo[]
-  todo?: ITodo
-}
+    message: string
+    status: string
+    todos: ITodo[]
+    todo?: ITodo
+  }
+  
