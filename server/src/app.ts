efc62d7e6ce2ff6c6d2bd23a0890bf4d04f784e4
Mathew Taylor <mathew.taylor@codebase8.com>
2020-12-29 12:22:20 +0000

    Update MongoDB connection strings

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ const PORT: string | number = process.env.PORT || 4000
 app.use(cors())
 app.use(todoRoutes)
 
-const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const uri: string = `mongodb://127.0.0.1:27017/${process.env.MONGO_DB}?readPreference=primary&appname=MongoDB%20Compass&ssl=false`
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.set('useFindAndModify', false)
 
