724ae5a5397556512dd8eba5bc1b8cc3152811cf
Mathew Taylor <mathew.taylor@codebase8.com>
2020-12-31 11:57:41 +0000

    Describe to test

--- server/src/controllers/ping/PingController.test.ts
+++ server/src/controllers/ping/PingController.test.ts
@@ -1,11 +1,9 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-describe('Test PingController', () => {
-  it('Request /ping should return Pong!', async () => {
-    const result = await request(app).get('/ping').send();
+test('Test PingController', async () => {
+  const result = await request(app).get('/ping').send();
 
-    expect(result.status).toBe(200);
-    expect(result.body.data).toBe('Pong!');
-  });
+  expect(result.status).toBe(200);
+  expect(result.body.data).toBe('Pong!');
 });
